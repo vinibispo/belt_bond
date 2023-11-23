8ccbc82d47d6815bd04735289ef3f50c1019a5fa
Vinícius Bispo <vbispo@vmox.com>
2023-11-22 23:41:30 -0300

    refactor: add keyring in auth_code

--- app/models/login/link.rb
+++ app/models/login/link.rb
@@ -1,11 +1,7 @@
 module Login::Link
   class << self
     def create(email)
-      auth_code = AuthCode.create!(
-        code: Faker::Internet.username(specifier: 8..12, separators: ['-']) + SecureRandom.hex(4),
-        expires_at: 5.minutes.from_now
-      )
-
+      auth_code = AuthCode.create!
       url = Rails.application.routes.url_helpers.verify_email_url(
         email:,
         code: auth_code.code
@@ -23,7 +19,12 @@ module Login::Link
 
       return unless params['code']
 
-      auth_code = AuthCode.lock.find_by("expires_at > datetime('now') and code = :code", code: params['code'])
+      code_digest = AuthCode.keyring.digest(params['code'])
+
+      auth_code = AuthCode
+                  .lock
+                  .where("expires_at > datetime('now')")
+                  .find_by(code_digest: code_digest)
 
       return unless auth_code
 
