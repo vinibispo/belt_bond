c662373ad760cbc8b44b095d159b0c6e53ed910e
Vinícius Bispo <vbispo@vmox.com>
2023-11-29 21:17:16 -0300

    chore: Update Encryptor module to accept a purpose for encryption/decryption

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -2,12 +2,12 @@ require 'active_support'
 module Encryptor
   class << self
 
-    def encrypt(unencrypted_message, expires_in: 10.minutes)
-      encryptor.encrypt_and_sign(unencrypted_message, expires_in: expires_in)
+    def encrypt(unencrypted_message, expires_in: 10.minutes, purpose: nil)
+      encryptor.encrypt_and_sign(unencrypted_message, expires_in:, purpose:)
     end
 
-    def decrypt(encrypted_message)
-      encryptor.decrypt_and_verify(encrypted_message)
+    def decrypt(encrypted_message, purpose: nil)
+      encryptor.decrypt_and_verify(encrypted_message, purpose:)
     end
 
     private
