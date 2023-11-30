6538a6e46189045b1a84b4a796587c56f97b8ff1
Vinícius Bispo <vbispo@vmox.com>
2023-11-29 21:15:34 -0300

    feat: Add AuthMailer and setup mail config

--- app/models/login.rb
+++ app/models/login.rb
@@ -8,8 +8,11 @@ class Login
   def call
     return [:error, 'Invalid email or password'] unless form.valid?
 
-    link = Link.create(email: form.email)
-    link.valid?(link)
+    login_link = Login::Link.create!(form.email)
+    encrypted_email = Encryptor.encrypt(form.email, purpose: :login)
+    encrypted_login_link = Encryptor.encrypt(login_link, purpose: :login)
+
+    AuthMailer.login(encrypted_email, encrypted_login_link).deliver_later
     [:ok, 'Login successful']
   end
 end
--- config/initializers/url_options.rb
+++ config/initializers/url_options.rb
@@ -1,6 +1,18 @@
-if Rails.env.test? || Rails.env.production?
+if Rails.env.development? || Rails.env.production?
   Rails.application.routes.default_url_options = {
-    host: "beltbond.com",
+    host: "dev.vinibispo.com",
     protocol: "https"
   }
+
+  Rails.application.config.action_mailer.default_url_options = Rails.application.routes.default_url_options
+end
+
+if Rails.env.test?
+  Rails.application.routes.default_url_options = {
+    host: "localhost",
+    protocol: "http",
+    port: 12_121
+  }
+
+  Rails.application.config.action_mailer.default_url_options = Rails.application.routes.default_url_options
 end
