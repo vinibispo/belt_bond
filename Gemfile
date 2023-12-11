b73839db8121d4635b7c1a844aa518245fb123be
vinibispo <vini.bispo015@gmail.com>
2023-12-11 20:43:14 -0300

    feat: Generate encrypted login link

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", ">= 4.0.1"
 
+gem "faker"
+
+gem "url_signature"
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- app/models/login/link.rb
+++ app/models/login/link.rb
@@ -1,15 +1,37 @@
 module Login::Link
   class << self
     def create(email)
-      
-      SignedURL.call(
-        url,
-        key: Rails.application.credentials.url_signature_secret,
-        expires: 2.minutes.from_now,
+      auth_code = AuthCode.create!(
+        code: Faker::Internet.username(specifier: 8..12, separators: ['-']) + SecureRandom.hex(4),
+        expires_at: 5.minutes.from_now
       )
+
+      url = Rails.application.routes.url_helpers.verify_email_url(
+        email:,
+        code: auth_code.code
+      )
+
+      SignedURL.call(url, key: Rails.application.credentials.signed_url_secret)
     end
 
-    def valid?(link)
+    def valid?(signed_url)
+      return unless SignedURL.verified?(signed_url, key: Rails.application.credentials.signed_url_secret)
+
+      uri = URI.parse(signed_url)
+
+      params = Rack::Utils.parse_query(uri.query)
+
+      return unless params['code']
+
+      auth_code = AuthCode.lock.find_by("expires_at > datetime('now') and code = :code", code: params['code'])
+
+      return unless auth_code
+
+      matched = ActiveSupport::SecurityUtils.secure_compare(auth_code.code, params['code'])
+
+      auth_code.destroy!
+
+      matched
     end
   end
 end
