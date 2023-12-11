2b80cced914aa2663e15c86b69e7ace40d26c040
vinibispo <vini.bispo015@gmail.com>
2023-12-11 20:43:15 -0300

    test: Add tests for AuthCode model's expired? method

--- test/models/auth_code_test.rb
+++ test/models/auth_code_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class AuthCodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "detects expired code" do
+    expires_at = 1.second.ago
+
+    assert AuthCode.new(expires_at:).expired?
+  end
+
+  test "detects fresh code" do
+    expires_at = 1.minute.from_now
+
+    refute AuthCode.new(expires_at:).expired?
+  end
 end
